faa059214b212b9b0e2f6f5770235210b3ccaadc
ODItina <primeunit2004@yahoo.com>
2014-10-03 11:49:49 +0100

    // Loash removed. Sub doc update service created

--- write.services/drug_regiment.js
+++ write.services/drug_regiment.js
@@ -1,7 +1,7 @@
 /**
  * Created by KP_TerminalUser2 on 02/10/2014.
  */
-var _ = require('lodash');
+
 var medicationSchema = require('../models/medication_model');
 var DrugRegiment = require('../dto/drug_regiment');
 
@@ -13,6 +13,26 @@ exports.create = function(args, next){
         }
         if(doc){
             doc.drug_regiment.push(drugRegiment);
+            doc.save(function(err,result){
+                if(err){
+                    next(err,null);
+                }
+                if(result){
+                    next(null,result);
+                }
+            });
+        }
+    });
+};
+
+exports.update = function(args, next){
+    var drugRegiment  = new DrugRegiment(args);
+    medicationSchema.findOne({patient_id: args.patient_id}, function(err, doc){
+        if(err){
+            next(err, null);
+        }
+        if(doc){
+            doc.drug_regiment.id(args._id).set(drugRegiment);
             doc.save(function(err,result){
                 if(err){
                     next(err,null);
